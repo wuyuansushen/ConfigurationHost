eead5389ce808ab6d6482931ccf70839a0bd6722
wuyuansushen <wuyuansushen@gmail.com>
2021-04-25 16:20:44 +0800

    Add project files.

---
+++ ConfigurationHost/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConfigurationHost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            await host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var myhost = Host.CreateDefaultBuilder(args);
+                myhost.ConfigureAppConfiguration(
+                 (hostcontext, config) =>
+                 {
+                     config.Sources.Clear();
+                     IHostEnvironment env = hostcontext.HostingEnvironment;
+
+                     config.AddJsonFile("appsettings.json", optional: false, true);
+                     config.AddJsonFile($"appsettins.{env.EnvironmentName}.json", true, true);
+                     IConfigurationRoot confRoot = config.Build();
+
+                     myhost.ConfigureServices((servicesCol)=> { servicesCol.Configure<TransientFaultHandlingOptions>(confRoot.GetSection(key: nameof(TransientFaultHandlingOptions))); });
+                 });
+            return myhost;
+        }
+    }
+}
---
+++ ConfigurationHost/TransientFaultHandlingOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationHost
+{
+    public class TransientFaultHandlingOptions
+    {
+        public bool Enabled { get; set; }
+        public TimeSpan AutoRetryDelay { get; set; }
+    }
+}
